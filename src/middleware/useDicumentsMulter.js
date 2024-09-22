e8a8654f3c3a991472d93977a7b6c1d91bbb8bdd
rendyariawan07052000@gmail.com <rendyariawan07052000@gmail.com>
2024-09-22 09:44:07 +0700

    fix multer

--- src/middleware/useDicumentsMulter.js
+++ src/middleware/useDicumentsMulter.js
@@ -9,27 +9,29 @@ const storage = multer.diskStorage({
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
     cb(null, uniqueSuffix + "-" + originalname);
   },
-  fileFilter: (req, file, cb) => {
-    const allowedMimeTypes = [
-      "application/pdf",
-      "application/zip",
-      "application/x-rar-compressed",
-      "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-      "application/msword",
-      "application/vnd.ms-excel",
-    ];
-
-    if (allowedMimeTypes.includes(file.mimetype)) {
-      cb(null, true);
-    } else {
-      cb(new Error("File type not allowed"), false);
-    }
-  },
 });
+
+const fileFilter = (req, file, cb) => {
+  const allowedMimeTypes = [
+    "application/pdf",
+    "application/zip",
+    "application/x-rar-compressed",
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    "application/msword",
+    "application/vnd.ms-excel",
+  ];
+
+  if (allowedMimeTypes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(new Error("File type not allowed"), false);
+  }
+};
 
 const uploadDokumen = multer({
   storage: storage,
+  fileFilter: fileFilter,
 });
 
 export default uploadDokumen;
--- src/middleware/useVideoMulter.js
+++ src/middleware/useVideoMulter.js
@@ -9,24 +9,26 @@ const storage = multer.diskStorage({
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
     cb(null, uniqueSuffix + "-" + originalname);
   },
-  fileFilter: (req, file, cb) => {
-    const allowedMimeTypes = [
-      "video/mp4", // MIME type untuk file .mp4
-      "video/x-msvideo", // MIME type untuk file .avi
-      "video/quicktime", // MIME type untuk file .mov
-      "video/x-matroska", // MIME type untuk file .mkv
-    ];
-
-    if (allowedMimeTypes.includes(file.mimetype)) {
-      cb(null, true);
-    } else {
-      cb(new Error("File type not allowed"), false);
-    }
-  },
 });
+
+const fileFilter = (req, file, cb) => {
+  const allowedMimeTypes = [
+    "video/mp4", // MIME type untuk file .mp4
+    "video/x-msvideo", // MIME type untuk file .avi
+    "video/quicktime", // MIME type untuk file .mov
+    "video/x-matroska", // MIME type untuk file .mkv
+  ];
+
+  if (allowedMimeTypes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(new Error("File type not allowed"), false);
+  }
+};
 
 const uploadVideos = multer({
   storage: storage,
+  fileFilter: fileFilter,
   // limits: {
   //     fileSize: 3 * 1000 * 1000 // 3 MB
   // }
